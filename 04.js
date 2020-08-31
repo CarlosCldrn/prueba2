2f3491b3e788a59e8f4f738edff60e8209f28a26
Halex <61364847+CarlosCldrn@users.noreply.github.com>
2020-08-31 02:40:29 -0600

    prueba

---
+++ 04.js
@@ -0,0 +1,39 @@
+const app= new Vue({
+    el: '#app',
+    data: {
+        saludo: 'Hola a todos, soy un ciclo de vida',
+        
+    },
+    methods:{
+        destruir(){
+            this.$destroy();
+        }
+    },
+    computed:{
+        
+    },
+    beforeCreate(){
+        console.log('beforeCreate');
+    },
+    created(){
+        console.log('created');
+    },
+    beforeMount(){
+        console.log('beforeMount');
+    },
+    mounted(){
+        console.log('mounted');
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate');
+    },
+    updated(){
+        console.log('updated');
+    },
+    beforeDestroy(){
+        console.log('beforeDestroy');
+    },
+    destroyed(){
+        console.log('destroyed');
+    },
+})
